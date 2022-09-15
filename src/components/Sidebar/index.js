579fb29bbde2bc28212eeacb3c4fb2da97cfecfe
Yanlin-Ma <yanlinm92@gmail.com>
2022-09-14 21:30:44 -0400

    WIP

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import sidebarIcon_1 from '../../assets/images/sidebar-1.svg';
 import sidebarIcon_2 from '../../assets/images/sidebar-2.svg';
 import sidebarIcon_3 from '../../assets/images/sidebar-3.svg';
+import Sidebar1 from '../svg/Sidebar1';
+import Sidebar2 from '../svg/Sidebar2';
+import Sidebar3 from '../svg/Sidebar3';
 import './styles.scss';
 
 const Sidebar = () => {
@@ -11,19 +14,19 @@ const Sidebar = () => {
       <ul>
         <li>
           <NavLink to="/monitoring" className='sidebar__link' activeClassName='is-active'>
-            <img src={sidebarIcon_1} />
+            <Sidebar1 />
             <div className='text'>Monitoring</div>
           </NavLink>
         </li>
         <li>
           <NavLink to="/control" className='sidebar__link' activeClassName='is-active'>
-            <img src={sidebarIcon_2} />
+            <Sidebar2 />
             <div className='text'>Control</div>
           </NavLink>
         </li>
         <li>
           <NavLink to="/workload" className='sidebar__link' activeClassName='is-active'>
-            <img src={sidebarIcon_3} />
+            <Sidebar3 />
             <div className='text'>Workload</div>
           </NavLink>
         </li>
---
+++ src/components/svg/Sidebar1.js
@@ -0,0 +1,13 @@
+const Sidebar1 = () => {
+  return (
+    <svg width="25" height="21" viewBox="0 0 25 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 1V20H24" stroke="#5F6368" stroke-width="1.8" stroke-linecap="round"/>
+      <path d="M3 13.4809L10.7384 7.60293L14.5781 11.7581L21.5781 6.25812" stroke="#5F6368" stroke-width="1.8" stroke-linecap="round"/>
+      <path d="M20.4615 5.67026L22.2831 5.35488L21.9949 7.26557L20.4615 5.67026Z" stroke="#5F6368"/>
+      <circle cx="10.5" cy="7.75812" r="1" stroke="#5F6368"/>
+      <circle cx="14.5" cy="11.7581" r="1" stroke="#5F6368"/>
+    </svg>
+  )
+}
+
+export default Sidebar1;
