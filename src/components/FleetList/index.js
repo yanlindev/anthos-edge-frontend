cf36bd2876f15978d870d2c22550a9c9a18869a7
Yanlin-Ma <yanlinm92@gmail.com>
2022-09-25 12:33:16 -0400

    WIP

--- src/components/FleetList/index.js
+++ src/components/FleetList/index.js
@@ -1,28 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import fleetInfoIcon from '../../assets/images/fleetInfo.svg';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import './styles.scss';
 import axios from 'axios';
-import $ from "jquery";
 
 const FleetList = () => {
+  const [fleetData, setFleetData] = useState([]);
+
   useEffect(() => {
     axios.get('https://edge-demo-fljjthbteq-uw.a.run.app/v1/abm/')
     .then(function (response) {
       // handle success
-      console.log(response);
+      console.log(response.data.length);
+      setTimeout(() => {
+        setFleetData(response.data);
+      }, 500);
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     })
-    // $.ajax({
-    //   url: "https://edge-demo-fljjthbteq-uw.a.run.app/v1/abm/",
-    //   type: 'GET',
-    //   success: function(res) {
-    //       console.log(res);
-    //       alert(res);
-    //   }
-  // });
   }, [])
 
   return (
@@ -32,11 +30,28 @@ const FleetList = () => {
         <div className='text'>Fleet Information</div>
       </div>
 
-      <div className='fleet-list__subtitle'>
-        <div className='text'>Name</div>
-        <div className='text'>Cluster</div>
-        <div className='text'>Version</div>
-      </div>
+      <table className='fleet-list__inner'>
+        <tr>
+          <th>Name</th>
+          <th>Cluster</th>
+          <th>Version</th>
+        </tr>
+        {
+          fleetData.length > 0 ?
+          fleetData.map(data => (
+            <tr>
+              <td>{data.location}</td>
+              <td>{data.node_count}</td>
+              <td>{data.version}</td>
+            </tr>
+          )) :
+          <SkeletonTheme>
+            <td><Skeleton count={8} height={30} /></td>
+            <td><Skeleton count={8} height={30} /></td>
+            <td><Skeleton count={8} height={30} /></td>
+          </SkeletonTheme>
+        }
+      </table>
     </div>
   )
 }
